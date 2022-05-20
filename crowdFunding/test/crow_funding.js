86ee0e6ad4796f052a401a1b5f90145d6f0b39ed
Tarun Sharma <tarunsharma@Sanchits-Mac-mini.local>
2022-05-20 18:35:02 +0530

    initial skaffold code commit for crowd funding contract

---
+++ crowdFunding/test/crow_funding.js
@@ -0,0 +1,13 @@
+const crowFunding = artifacts.require("crowFunding");
+
+/*
+ * uncomment accounts to access the test accounts made available by the
+ * Ethereum client
+ * See docs: https://www.trufflesuite.com/docs/truffle/testing/writing-tests-in-javascript
+ */
+contract("crowFunding", function (/* accounts */) {
+  it("should assert true", async function () {
+    await crowFunding.deployed();
+    return assert.isTrue(true);
+  });
+});
